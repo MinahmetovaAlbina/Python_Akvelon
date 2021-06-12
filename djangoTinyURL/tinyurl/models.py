df57d395c2bedd2d672748462799d06fee115832
MinahmetovaAlbina <wildflow14er@gmail.com>
2021-06-13 00:20:27 +0300

    add model MyUrl

--- djangoTinyURL/tinyurl/models.py
+++ djangoTinyURL/tinyurl/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class MyUrl(models.Model):
+    original_url = models.CharField(max_length=200)
+    tiny_url = models.CharField(max_length=200, default=original_url)
+    pub_date = models.DateTimeField('date published', default=timezone.now())
+    last_us_date = models.DateTimeField('date of last using of url', default=pub_date)
+    num_of_uses = models.IntegerField('number of times tiny url was used', default=0)

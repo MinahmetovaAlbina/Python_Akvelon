2a19d6b8a7dab1f7fd48f7b0eb0d759a082f44c2
MinahmetovaAlbina <wildflow14er@gmail.com>
2021-06-13 13:45:53 +0300

    add admin, fix model MyUrl, add __str__ for MyUrl

--- djangoTinyURL/tinyurl/models.py
+++ djangoTinyURL/tinyurl/models.py
@@ -4,7 +4,10 @@ from django.utils import timezone
 
 class MyUrl(models.Model):
     original_url = models.CharField(max_length=200)
-    tiny_url = models.CharField(max_length=200, default=original_url)
-    pub_date = models.DateTimeField('date published', default=timezone.now())
-    last_us_date = models.DateTimeField('date of last using of url', default=pub_date)
+    tiny_url = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    last_us_date = models.DateTimeField('date of last using of url')
     num_of_uses = models.IntegerField('number of times tiny url was used', default=0)
+
+    def __str__(self):
+        return self.tiny_url

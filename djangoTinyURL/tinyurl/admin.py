735cea9afdac3a5dd996af6d8e37ee9653e0c67d
MinahmetovaAlbina <wildflow14er@gmail.com>
2021-06-14 02:50:45 +0300

    upgrade Admin

--- djangoTinyURL/tinyurl/admin.py
+++ djangoTinyURL/tinyurl/admin.py
@@ -4,4 +4,13 @@ from django.contrib import admin
 from .models import MyUrl
 
 
-admin.site.register(MyUrl)
+class TinyUrlAdmin(admin.ModelAdmin):
+    list_display = ('original_url', 'tiny_url', 'num_of_uses')
+    search_fields = ['original_url']
+    list_filter = ['num_of_uses']
+
+
+admin.site.register(MyUrl, TinyUrlAdmin)
+admin.AdminSite.site_header = 'Tiny URL Administrations'
+admin.AdminSite.site_title = 'Tiny URL'
+

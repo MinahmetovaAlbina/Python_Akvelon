815396bb6a2433b28328bdd71cbcd588b8964b2d
MinahmetovaAlbina <wildflow14er@gmail.com>
2021-06-14 22:10:50 +0300

    fix bugs on Create veiw and add comments

--- djangoTinyURL/tinyurl/views.py
+++ djangoTinyURL/tinyurl/views.py
@@ -1,9 +1,9 @@
 from django.db.models import F
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import render
 from django.utils import timezone
 from django.views import generic
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse
 
 from .models import MyUrl
 from .hash import get_hash
@@ -13,6 +13,7 @@ class IndexView(generic.ListView):
     template_name = 'tinyurl/index.html'
     context_object_name = 'most_frequently_used'
 
+    # set of all MyUrls sorted by number of uses
     def get_queryset(self):
         return MyUrl.objects.order_by('-num_of_uses')
 
@@ -30,28 +31,41 @@ def create(request):
                 'error_message': "Key error"
             })
     else:
-        duplicate_url = MyUrl.objects.filter(original_url=original_url).first()
-        if duplicate_url is not None:
-            return render(request, 'tinyurl/create.html', {
-                'error_message': "I already made a tiny url for this one"
-            })
-        elif original_url is None or original_url is '':
+        # if the original url is empty
+        if original_url is None or original_url is '':
             return render(request, 'tinyurl/create.html', {
                 'error_message': "You didn't give me any url ;("
             })
+
         else:
-            my_url = MyUrl.objects.create(original_url=original_url, hash=get_hash(original_url),
-                                          pub_date=timezone.now(), last_us_date=timezone.now(), num_of_uses=0)
-            my_url.save()
-            return HttpResponseRedirect(reverse('tinyurl:detail', args=(my_url.id,)))
+            # find this original url in DB
+            duplicate_url = MyUrl.objects.filter(original_url=original_url).first()
+            # if MyUrl with this original_url already exists
+            if duplicate_url is not None:
+                return render(request, 'tinyurl/create.html', {
+                    'error_message': "I already made a tiny url for this one"
+                })
+            else:
+                # generate a MyUrl
+                my_url = MyUrl.objects.create(original_url=original_url, hash=get_hash(original_url),
+                                              pub_date=timezone.now(), last_us_date=timezone.now(), num_of_uses=0)
+                # save the changes in DB
+                my_url.save()
+                # redirect to the detail page
+                return HttpResponseRedirect(reverse('tinyurl:detail', args=(my_url.id,)))
 
 
+# redirects to the original url from the tiny url
 def tiny_url(request, my_hash):
     my_url = MyUrl.objects.filter(hash=my_hash).first()
+    # if MyUrl with the given hash doesn't exist
     if my_url is None:
         raise Http404("Page does not exist")
     else:
+        # increase number of uses
         my_url.num_of_uses = F('num_of_uses') + 1
+        # update the last use date
         my_url.last_us_date = timezone.now()
+        # save the changes in DB
         my_url.save()
         return HttpResponseRedirect(my_url.original_url)
